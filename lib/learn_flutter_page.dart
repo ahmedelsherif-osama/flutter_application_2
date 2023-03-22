4f6bf86090e8022b25f6b24b9ce1fb2f752cd6b7
Ahmed Elsherif <ahmedelsherif.osama@gmail.com>
2023-03-22 04:25:32 +0400

    Initial commit

---
+++ lib/learn_flutter_page.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+
+class LearnFlutterPage extends StatefulWidget {
+  const LearnFlutterPage({super.key});
+
+  @override
+  State<LearnFlutterPage> createState() => _LearnFlutterPageState();
+}
+
+class _LearnFlutterPageState extends State<LearnFlutterPage> {
+  bool isSwitch = false;
+  bool? isCheckBox = false;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Learn Flutter"),
+        automaticallyImplyLeading: false,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.of(context).pop();
+            },
+            icon: const Icon(Icons.arrow_back_ios)),
+        actions: [
+          IconButton(
+            onPressed: () {
+              debugPrint("AppBar Action");
+            },
+            icon: const Icon(Icons.info_outline),
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(height: 200, child: Image.asset("images/einstein.jpg")),
+            const SizedBox(
+              height: 10,
+            ),
+            const Divider(color: Colors.black),
+            Container(
+              margin: const EdgeInsets.all(10.0),
+              padding: const EdgeInsets.all(10.0),
+              color: Colors.blueGrey,
+              width: double.infinity,
+              child: const Center(
+                child: Text(
+                  "This is a text widget",
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                primary: isSwitch ? Colors.green : Colors.blue,
+              ),
+              onPressed: () {
+                debugPrint("Elevated Button");
+              },
+              child: const Text("Elevated Button"),
+            ),
+            OutlinedButton(
+              onPressed: () {
+                debugPrint("Outlined Button");
+              },
+              child: const Text("Outlined Button"),
+            ),
+            TextButton(
+              onPressed: () {
+                debugPrint("Text Button");
+              },
+              child: const Text("Text Button"),
+            ),
+            GestureDetector(
+              onTap: () {
+                debugPrint("This is the row");
+              },
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: const [
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                  Text("Row Widget"),
+                  Icon(
+                    Icons.local_fire_department,
+                    color: Colors.blue,
+                  ),
+                ],
+              ),
+            ),
+            Switch(
+                value: isSwitch,
+                onChanged: (bool newBool) {
+                  setState(() {
+                    isSwitch = newBool;
+                  });
+                }),
+            Checkbox(
+                value: isCheckBox,
+                onChanged: (bool? newBool) {
+                  setState(() {
+                    isCheckBox = newBool;
+                  });
+                }),
+            Image.network(
+                "https://cref.it/wp-content/uploads/2021/05/0e447f69-25e3-4f0f-9ad7-146a85278d6b.jpg")
+          ],
+        ),
+      ),
+    );
+  }
+}
